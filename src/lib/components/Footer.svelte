f71ce1e7f758c247913d7c325c59e74178c5c0de
Adria Blancafort <adriablancafort@gmail.com>
2024-11-09 09:30:06 +0100

    add footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,7 @@
+<footer class="border-t border-neutral-300">
+    <nav class="container flex items-center px-3 py-4 mx-auto sm:px-6">
+        <a href="/">
+            <img src="https://cdn.asilor.com/asilor-logo-black.svg" alt="Asilor Logo" width="129" height="33">
+        </a>
+    </nav>
+</footer>
