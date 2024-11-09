e59e1fc7a426048b96456d84046cef7b006752aa
Adria Blancafort <adriablancafort@gmail.com>
2024-11-09 13:43:31 +0100

    overflow hidden on layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script>
-	import '../app.css';
 	import Header from "$lib/components/Header.svelte";
+	import Footer from "$lib/components/Footer.svelte";
+
+	import '../app.css';
 	let { children } = $props();
 </script>
 
 <Header />
-
-<main class="container px-3 py-4 mx-auto sm:px-6">
+<main class="container relative px-3 py-4 mx-auto overflow-x-hidden sm:px-6">
 	{@render children()}
 </main>
+<Footer />
