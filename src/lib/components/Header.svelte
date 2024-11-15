7355b7c477fc7a4b34c837dd4c3a9a0b4cda3be0
Adria Blancafort <adriablancafort@gmail.com>
2024-11-15 02:42:33 +0100

    a11y: fix button

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -21,7 +21,7 @@
 </script>
 
 {#snippet menuToggle()}
-  <div class="mr-2 md:ml-4 rounded-full h-9 w-9 bg-neutral-300/25 text-white flex items-center justify-center cursor-pointer" onclick={() => openMenu = !openMenu}>
+  <button class="mr-2 md:ml-4 rounded-full h-9 w-9 bg-neutral-300/25 text-white flex items-center justify-center cursor-pointer" onclick={() => openMenu = !openMenu}>
     {#if openMenu}
         <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" fill="none" class="h-5 w-5">
             <path fill="currentColor" fill-rule="evenodd" d="M16.293 17.707a1 1 0 001.414-1.414L11.414 10l6.293-6.293a1 1 0 00-1.414-1.414L10 8.586 3.707 2.293a1 1 0 00-1.414 1.414L8.586 10l-6.293 6.293a1 1 0 101.414 1.414L10 11.414l6.293 6.293z"/>
@@ -31,7 +31,7 @@
             <path fill="currentColor" fill-rule="evenodd" d="M19 4a1 1 0 01-1 1H2a1 1 0 010-2h16a1 1 0 011 1zm0 6a1 1 0 01-1 1H2a1 1 0 110-2h16a1 1 0 011 1zm-1 7a1 1 0 100-2H2a1 1 0 100 2h16z"/>
         </svg>
     {/if}
-  </div>
+  </button>
 {/snippet}
 
 {#snippet localeToggle()}
